f3b368e99654b28bb16e7f208e7c954e7bfb12d7
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-09-26 19:13:57 -0400

    initial commit

---
+++ provisioner.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+apt-get update
+
+#install any binaries for compiling from source
+apt-get install -y build-essential
+
+#install git-scm
+apt-get install -y git
+
+#install utilities
+apt-get install -y unzip
+
+sudo timedatectl set-timezone America/New_York
+
+#install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+
+#allow non-root user to invoke a Node server on ports < 1024s
+sudo apt-get install libcap2-bin
+sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
+
+#install mySql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234'
+
+apt-get install -y mysql-server
