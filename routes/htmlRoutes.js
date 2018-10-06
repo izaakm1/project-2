0e8bbc02583047f34a7dbc39fbb75f2df34d6ca8
lukia <xhumen@msn.com>
2018-10-06 10:31:41 -0400

    user handlebars added html route surports css works

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -37,6 +37,15 @@ module.exports = function(app) {
     res.render("contactus")
   })
 
+  app.get("/user", function(req, res) {
+    db.user.findAll({}).then(function(dbusers) {
+      res.render("user", {
+        msg: "Welcome!",
+        user: dbusers
+      });
+    });
+  });
+
   
 
   // Render 404 page for any unmatched routes
