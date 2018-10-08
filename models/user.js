71ef12a1b7d8f390d89c51277e8f4c34c7ee5956
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-10-08 16:19:38 -0400

    udpates to routing / tables

--- models/user.js
+++ models/user.js
@@ -1,53 +1,57 @@
-module.exports = function(sequelize, Sequelize) {
- 
+module.exports = function (sequelize, Sequelize) {
+
     var User = sequelize.define('user', {
- 
+
         id: {
             autoIncrement: true,
             primaryKey: true,
             type: Sequelize.INTEGER
         },
- 
+
         firstname: {
             type: Sequelize.STRING,
             notEmpty: true
         },
- 
+
         lastname: {
             type: Sequelize.STRING,
             notEmpty: true
         },
- 
+
         username: {
             type: Sequelize.TEXT
         },
- 
+
         about: {
             type: Sequelize.TEXT
         },
- 
+
         email: {
             type: Sequelize.STRING,
             validate: {
                 isEmail: true
             }
         },
- 
+
         password: {
             type: Sequelize.STRING,
             allowNull: false
         },
- 
+
         last_login: {
             type: Sequelize.DATE
         },
- 
+
         status: {
             type: Sequelize.ENUM('active', 'inactive'),
             defaultValue: 'active'
         },
 
         business: {
+            type: Sequelize.TEXT
+        },
+
+        address: {
             type: Sequelize.TEXT
         },
 
@@ -62,9 +66,34 @@ module.exports = function(sequelize, Sequelize) {
         zip: {
             type: Sequelize.TEXT
         },
- 
- 
+
+        monday: {
+            type: Sequelize.TEXT
+        },
+        tuesday: {
+            type: Sequelize.TEXT
+        },
+        wednesday: {
+            type: Sequelize.TEXT
+        },
+        thursday: {
+            type: Sequelize.TEXT
+        },
+        friday: {
+            type: Sequelize.TEXT
+        },
+        saturday: {
+            type: Sequelize.TEXT
+        },
+        sunday: {
+            type: Sequelize.TEXT
+        },
+
     });
- 
+
+    // User.associate = function (models) {
+    //     User.hasOne(models.Example)
+    // }
+
     return User;
 }
